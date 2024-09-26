80b1bead1a4fd2802ec0f65bc0f54003a505e17b
cuongnbph22662 <bacuong15423@gmail.com>
2024-09-26 15:01:09 +0700

    commit

--- backend/test/MsfServer.Application.Tests/RepositorysTests/TokenRepositorys/DeleteTokenAsync_Tests.cs
+++ backend/test/MsfServer.Application.Tests/RepositorysTests/TokenRepositorys/DeleteTokenAsync_Tests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Moq;
 using MsfServer.Application.Contracts.Token.TokenDtos;
 using MsfServer.Application.Repositorys;
+using MsfServer.Domain.Entities;
 using MsfServer.Domain.Shared.Exceptions;
 using System.Data;
 using System.Threading.Tasks;
@@ -40,9 +41,10 @@ namespace MsfServer.Application.Tests.RepositorysTests.TokenRepositorys
         [Fact]
         public async Task DeleteTokenAsync_ShouldReturnSuccessResponse_WhenTokenDeleted()
         {
-            // Arrange
             string validUserId = "1";
-            await Assert.ThrowsAsync<CustomException>(() => _repository.DeleteTokenAsync(validUserId));
+            var result = await _repository.DeleteTokenAsync(validUserId);
+            Assert.Equal("Xóa token thành công.", result.Message);
+            Assert.Equal(StatusCodes.Status204NoContent, result.Status);
         }
     }
 }
